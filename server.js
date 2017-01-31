f9d1873e5d212a2aae689c03e7252a40d4540e75
SashaKoro <balaskorotkov@gmail.com>
2017-01-31 12:15:21 -0500

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 let app = express();
 const { dateChecker } = require('./dateChecker');
-
+const port = process.env.PORT || 2000;
 
 app.get('/:date', (req, res) => {
   let incomingDate = req.params.date;
@@ -18,5 +18,6 @@ app.get('/', (req, res) => {
   res.render( 'intro.hbs');
 });
 
-app.listen(2000);
-console.log('Starting app on port 2000');
+app.listen(port, () => {
+  console.log(`Starting app on port ${port}`);
+});
